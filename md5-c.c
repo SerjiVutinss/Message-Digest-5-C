2d32da5ff5c5b52ffc971ecbb2d1c3383392ce77
SerjiVutinss <SerjiVutinss@gmail.com>
2020-03-05 12:10:02 +0000

    More tidy up

--- md5-c.c
+++ md5-c.c
@@ -7,6 +7,7 @@
 #include "md5-c.h"
 #include "sha256.h"
 
+
 void printHashInfo(HashInfo *h) {
 
     printf("-------------------------------------------------------------\n");
@@ -15,27 +16,15 @@ void printHashInfo(HashInfo *h) {
     printf("                Total Bytes Needed: %d\n", h->totalBytesNeeded);
     printf("-------------------------------------------------------------\n");
     printf("                       Full Blocks: %ld\n", h->fullBlocks);
-    printf("                     Padded Blocks: %ld\n", h->paddedBlocks);
-    printf("        Input Bytes in Full Blocks: %ld\n", h->bytesInFullBlocks);
-    printf(" Input Bytes in First Padded Block: %d\n", h->bytesInPaddedBlocks);
     printf("-------------------------------------------------------------\n");
 }
 
 void calculateHashInfo(HashInfo *h, long inputBytes) {
-
     printf("\nCalculating hash info...\n");
-
     HashInfo hashInfo;
-
     h->inputBytes = inputBytes;
     h->totalBytesNeeded = inputBytes + RESERVED_BYTES;
     h->fullBlocks = floor((double)(inputBytes) / BLOCK_SIZE);
-
-    h->bytesInFullBlocks = h->fullBlocks * BLOCK_SIZE;
-    int remainingInputBytes = h->inputBytes - h->bytesInFullBlocks;
-    h->bytesInPaddedBlocks = remainingInputBytes < 0 ? remainingInputBytes + 64: remainingInputBytes;
-
-    h->paddedBlocks = h->bytesInPaddedBlocks < 56 ? 1 : 2;
 }
 
 long getFileBytes(FILE *file) {
@@ -45,6 +34,12 @@ long getFileBytes(FILE *file) {
     fseek(file, 0L, 0L);
 
     return numInputBytes;
+}
+
+void getFileInfo(FILE *file, HashInfo *h)  {
+    calculateHashInfo(h, getFileBytes(file));
+    printHashInfo(h);
+    
 }
 
 void convertBlockToHostEndianness(BLOCK *block){
@@ -65,18 +60,10 @@ void createFullyPaddedBlock(BLOCK *block, uint64_t len) {
     block->sixty_four[7] = htobe64(len);
 }
 
-// typedef union {
-//   uint64_t sixfour[8];
-//   uint32_t threetwo[16];
-//   uint8_t eight[64];
-// } BLOCK;
-
 int main(int argc, char *argv[]) {
 
     printf("--- MD5 Algorithm---\n");
     printf("Block Size: %d Bytes\n\n", BLOCK_SIZE);
-
-    // char fileName[] = "./res/two-pad-block.txt";
 
     // Expect and open a single filename.
     if (argc != 2) {
@@ -89,23 +76,9 @@ int main(int argc, char *argv[]) {
         printf("Error: couldn't open file %s.\n", argv[1]);
         return 1;
     }
-    // FILE *inFile = fopen(fileName, "rb");
-
-    // Open the file
-    // printf("Opening file: %s\n", fileName);
-
-    long fileBytes = getFileBytes(inFile);
-    printf("\tBytes in file: %ld\n", fileBytes);
 
     HashInfo hashInfo;
-    calculateHashInfo(&hashInfo, fileBytes);
-    printHashInfo(&hashInfo);
-
-
-    char *buffer; // read in to this buffer from the file.
-    // If there are any full blocks, allocate the buffer right now.
-    // It will be reallocated for the padded block later.
-    if(hashInfo.fullBlocks > 0) buffer = (char*)malloc(64 * sizeof(char));
+    getFileInfo(inFile, &hashInfo);
 
     printf("\n");
     printf("Processing: %ld full blocks...\n", hashInfo.fullBlocks);
@@ -120,20 +93,18 @@ int main(int argc, char *argv[]) {
 
     uint64_t len = (uint64_t)hashInfo.inputBytes * 8ULL;
 
-    // for each full block...
+    // Process the full blocks
     for(int i = 0; i < hashInfo.fullBlocks; i++){
         // Read all 64 bytes for this block
         size_t read = fread(M.eight, 1, 64, inFile);
-        // convertBlockToHostEndianness(&M);
-        // nextHashSHA256(M.thirty_two, H);
         closeBlock(&M, H);
     }
     printf("\tAll full blocks processed\n");
 
     // Padded block - in all cases, all remaining input bytes AND 1bit will fit in this block.
     printf("Processing Remaining Input Bytes:\n");
-    // read the last bytes from the file
-    size_t bytesInBlock = fread(M.eight, 1, hashInfo.bytesInPaddedBlocks, inFile);
+    // Read the last bytes from the file, knowing that it is less than 64.
+    size_t bytesInBlock = fread(M.eight, 1, 64, inFile);
     printf("\tBytes read: %d\n", bytesInBlock);
     // Since this is not a full block, there must be room for at least one more byte, so add the 1bit.
     M.eight[bytesInBlock] = 0x80;
@@ -146,24 +117,18 @@ int main(int argc, char *argv[]) {
             M.eight[i] = 0x00;
         }
         M.sixty_four[7] = htobe64(len);
-        // convertBlockToHostEndianness(&M);
-        // nextHashSHA256(M.thirty_two, H);
         closeBlock(&M, H);
     }
     else {
         printf("\tTwo padded blocks\n");
         // Pad the rest of this block with zeroes and add a new fully padded block
-        // First, hash the previous block
         for(int i = bytesInBlock; i < 64; i++){
             M.eight[i] = 0x00;
         }
-        convertBlockToHostEndianness(&M);
-        nextHashSHA256(M.thirty_two, H);
+        closeBlock(&M, H);
         // Now, create the fully padded block and also hash it
         printf("\n\tCreating new block\n");
         createFullyPaddedBlock(&M, len);
-        // convertBlockToHostEndianness(&M);
-        // nextHashSHA256(M.thirty_two, H);
         closeBlock(&M, H);
     }
 
@@ -171,8 +136,6 @@ int main(int argc, char *argv[]) {
     // Print the hash.
     for (int i = 0; i < 8; i++) {
         printf("%08" PRIx32 "", H[i]);
-
-        // fprintf(outfile, "%08" PRIx32 "", H[i]);
     }
 
     printf("\n");
--- md5-c.h
+++ md5-c.h
@@ -7,27 +7,12 @@ const int BLOCK_SIZE = 512 / 8;
 const int RESERVED_BYTES = 8;
 
 typedef struct HashInfo {
-
     // User set members
     long inputBytes;
-
     // Calculated members
     long totalBytesNeeded;
-
     // Blocks completely filled with input bytes
     long fullBlocks;
-    int paddedBlocks;
-
-    int bytesNotInFullBlocks;
-
-
-    long bytesInFullBlocks;
-    int bytesInLastBlock;
-
-    int bytesInPaddedBlocks;
-
-    int hasFinalEmptyBlock;
-
 } HashInfo;
 
 #endif
